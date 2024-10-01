451cbe3b8a03da2c6d4c4f615cff0d1353309eaa
kingno3l <nuelluiz@gmail.com>
2024-10-01 19:53:12 +0100

    implemented radio button

--- app/src/main/java/android/example/composejoshua/MainActivity.kt
+++ app/src/main/java/android/example/composejoshua/MainActivity.kt
@@ -12,8 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.example.composejoshua.ui.theme.ComposeJoshuaTheme
+import android.widget.Toast
+import androidx.benchmark.perfetto.Row
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Icon
@@ -25,7 +28,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.runtime.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.RadioButton
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -55,11 +60,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun MyRadio(){
+    val clickRadio = LocalContext.current
+
+    Row(verticalAlignment = Alignment.CenterVertically){
+        RadioButton(
+            selected = false,
+            onClick = {
+                Toast.makeText(
+                    clickRadio,
+                    "I am a Youth for Jesus",
+                    Toast.LENGTH_LONG
+                ).show()
+            })
+        Text(text = "Youth")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposeJoshuaTheme {
-        Greeting("Android")
+        MyRadio()
     }
 }
