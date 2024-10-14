713ca406fdfee3bf13ab9badf19303c175d55feb
kingno3l <nuelluiz@gmail.com>
2024-10-14 20:16:39 +0100

    higher order function implemented

--- app/src/main/java/android/example/composejoshua/LambdaaHOP.kt
+++ app/src/main/java/android/example/composejoshua/LambdaaHOP.kt
@@ -5,13 +5,24 @@ fun main() {
 //    hof(add)
 
     hof {a: Int, b: Int -> a+b}
+    hof2("Generation")  {a: Int, b: Int -> a+b}
 
 }
+
+
 
 fun hof(addition: (Int, Int) -> Int) {
     val result = addition(4,6)
     println(result)
 }
+
+fun hof2(name:String, addition: (Int, Int) -> Int){
+    val result = addition(4,6)
+    println("Joshua $name : " + result)
+}
+
+
+
 
 
 
