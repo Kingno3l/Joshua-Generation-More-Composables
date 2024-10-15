100d38d15663940bc463e909dbcda64106a9aa40
kingno3l <nuelluiz@gmail.com>
2024-10-15 19:48:31 +0100

    lazyvertical grid done

--- app/src/main/java/android/example/composejoshua/MainActivity.kt
+++ app/src/main/java/android/example/composejoshua/MainActivity.kt
@@ -25,6 +25,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
@@ -66,8 +68,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -121,19 +125,43 @@ fun PopulateItems(osList: List<String>) {
 //        }
 //    }
 
-    LazyRow(){
-        item {
-            Text(text = "Joshua first item ")
-        }
+//    LazyRow(){
+//        item {
+//            Text(text = "Joshua first item ")
+//        }
+//
+//        items(8){ index ->
+//            Text(text = "item: $index")
+//        }
+//
+//        items(osList){
+//            Text(text = "Name of OS: $it")
+//        }
+//    }
 
-        items(8){ index ->
-            Text(text = "item: $index")
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        content = {
+            items(osList.size){
+                index -> Card(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxWidth(),
+                    elevation = CardDefaults
+                    .cardElevation(8.dp),
+                    colors = CardDefaults
+                        .cardColors(Color.Blue)
+                ){
+                  Text(text = osList[index],
+                      fontSize = 20.sp,
+                      color = Color.White,
+                      textAlign = TextAlign.Center,
+                      fontWeight = FontWeight.Bold
+                  )
+            }
+            }
         }
-
-        items(osList){
-            Text(text = "Name of OS: $it")
-        }
-    }
+    )
 }
 
 @Composable
